6b7a3ff9203dabb5c98e2b583204d22c6ccff5c1
yashrn <157579526+yashrn@users.noreply.github.com>
2025-07-03 13:41:22 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import Menubar from "./components/menubar.jsx";
 import Home from "./pages/Home.jsx";
+import Footer from "./components/Footer.jsx";
+import {Route,Routes} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
-      <Menubar /> 
-      <Home />
+      <Menubar />
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
