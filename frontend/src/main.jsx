bd6248ed5c7a28096807d767741527afd4f141d3
yashrn <157579526+yashrn@users.noreply.github.com>
2025-07-10 14:10:02 +0530

    chore(auth): set up ClerkProvider in main.jsx with env config
    Integrated Clerk authentication by wrapping the App component with ClerkProvider in main.jsx. 
    Also added a publishable key check using VITE_CLERK_PUBLISHABLE_KEY from the .env file.
    
    This sets up the base for using Clerk's sign-in, sign-up, and user management features across the app.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,19 @@ import './index.css'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if(!PUBLISHABLE_KEY){
+  throw new Error('Missing publishable key')
+  
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-      <App />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
   </BrowserRouter>
 )
